b2ff9697012c414c9b15770c55b6ed6eff18276e
Monarcamich <2509016@upy.edu.mx>
2026-07-03 11:16:26 -0600

    Uploded Changes Classwork-11

--- Classwork-11-The-Mandelbrot-Set/mandelbrot_set_math.py
+++ Classwork-11-The-Mandelbrot-Set/mandelbrot_set_math.py
@@ -1,8 +1,9 @@
-config={}
-file = open ('config.txt','r')
+config = {}
+
+file = open("/Classwork-11 ","config.txt", "r")
 
 for line in file:
-    parameter, value =line.strip().split('=')
+    parameter, value = line.strip().split("=")
     config[parameter] = float(value) if "." in value else int(value)
 file.close()
 
@@ -11,34 +12,18 @@ width, height, max_iter = config["ancho"], config["alto"], config["max_iter"]
 output = open("mandelbrot.csv", "w")
 output.write("row,column,iterations\n")
 
-
-ancho = config['ancho']
-alto = config['alto']
-max_iter = config['max_iter']
-'''
-Formula:
-
-real = real_min + (columna / ancho) * (real_max - real_min)
-imag = imag_min + (fila /alto) * (imag_max - imag_min)
-c = complex (real, imag)
-
-'''
-for fila in range(alto):
-    for columna in range(ancho):
-        real = config['real_min'] + (columna / ancho) * (config['real_max'] - config['real_min'])
-        imag = config['imag_min'] + (fila / alto) * (config['imag_max'] - config['imag_min'])      
-        c = complex (real, imag)
-
-for fila in range (alto):
-    for columna in range(ancho):
+for row in range(height):
+    for column in range(width):
+        real = config["real_min"] + (column / width) * (config["real_max"] - config["real_min"])
+        imag = config["imag_min"] + (row / height) * (config["imag_max"] - config["imag_min"])
+        c = complex(real, imag)
+        
         z = 0 + 0j
-        iteraciones = 0
-
-        while abs(z) < 2 and iteraciones < config['max_iter']:
-            z = z * z +c
-            iteraciones += 1
-        output.write(f"{columna} {fila} {iteraciones}\n")
-
-print("DONE")
-
-
+        iterations = 0
+        
+        while (abs(z) <=2) and (iterations < max_iter):
+            z = z * z + c
+            iterations += 1
+        
+        output.write(f"{row},{column},{iterations}\n")
+print|("DONE")
